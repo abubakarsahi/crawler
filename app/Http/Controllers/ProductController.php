6adbc45ca84a8768505540b9b57e351aa3a1f982
haroonkhan <haroon@rocketdrop.com>
2022-04-29 12:13:38 +0500

    initial project

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Goutte\Client;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $client = new Client();
+        $crawler = $client->request('GET', 'https://github.com/');
+        dd($crawler->selectLink('Sign in')->link());
+        $crawler = $client->click($crawler->selectLink('Sign in')->link());
+        $form = $crawler->selectButton('Sign in')->form();
+        $crawler = $client->submit($form, ['login' => 'fabpot', 'password' => 'xxxxxx']);
+        $crawler->filter('.flash-error')->each(function ($node) {
+            print $node->text()."\n";
+        });
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
